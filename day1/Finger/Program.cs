33c49cb45e89a89e9cb031c5de93236140aad7eb
Jared Zahn <jared_zahn@progressive.com>
2023-02-06 15:42:05 -0500

    get, post

--- day1/Finger/Program.cs
+++ day1/Finger/Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.MapGet("/status", () => {
+    var status = new StatusMessage("All is good.", DateTimeOffset.Now);
+    return status;
+});
+
+app.MapPost("/status", (StatusRequest req) => {
+    return req;
+});
+
+
 app.Run();
 
-
+public record StatusRequest(string Message);
