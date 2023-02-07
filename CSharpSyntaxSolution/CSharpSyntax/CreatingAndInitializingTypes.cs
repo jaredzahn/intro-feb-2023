bb54e76126b98d1fcc6aecc2b386bf1e8fadaf29
Jared Zahn <jared_zahn@progressive.com>
2023-02-07 11:52:10 -0500

    variables.

---
+++ CSharpSyntaxSolution/CSharpSyntax/CreatingAndInitializingTypes.cs
@@ -0,0 +1,41 @@
+﻿
+namespace CSharpSyntax;
+
+public class CreatingAndInitializingTypes
+{
+    [Fact]
+    public void UsingLiteralsToCreateInstancesOfTypes()
+    {
+        //Initialize:
+        string myName = "Jared";
+        int myAge = 22;
+        bool isLegalAgeToDrink = myAge >= 21;
+        bool isVendor = true;
+
+        //Act:
+        Taco food = new Taco();
+
+        //Assert:
+        Assert.Equal("Jared", myName);
+        Assert.Equal(22, myAge);
+        Assert.True(isLegalAgeToDrink);
+    }
+
+    [Fact]
+    public void ImplicitlyTypedLocalVariables()
+    {
+        // var can be used for local variables, and you must initialize the variable.
+        var myLevel = 100;
+
+        var myName = "Jared";
+
+        var favoriteFood = new Taco();
+
+        var myPay = 25.23M;
+
+        //option in c# 6, pretty obscure:
+        Taco lunch = new();
+    }
+}
+
+public class Taco { }
--- day2/Demo1Solution/Demo1.UnitTests/UnitTest1.cs
+++ day2/Demo1Solution/Demo1.UnitTests/UnitTest1.cs
@@ -2,12 +2,7 @@ namespace Demo1.UnitTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-            
-        }
-
+       
         [Fact]
         public void CanAddTwoNumbers()
         {
@@ -17,6 +12,16 @@ namespace Demo1.UnitTests
             c = a + b;
             //Then (Assert)
             Assert.Equal(30, c);
+        }
+
+        [Theory]
+        [InlineData(2,2,4)]
+        [InlineData(8, 2, 10)]
+        [InlineData(40, 2, 42)]
+        public void CanAddTwoNumberTheory(int a, int b, int expected)
+        {
+            int answer = a + b;
+            Assert.Equal(expected, answer);
         }
     }
 }
