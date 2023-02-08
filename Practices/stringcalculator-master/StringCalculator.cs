8a7999433c2249bdd7ef35891b83c21af4febabf
Jared Zahn <jared_zahn@progressive.com>
2023-02-08 15:21:14 -0500

    String Calculator Practice Exercise Implemented.

---
+++ Practices/stringcalculator-master/StringCalculator.cs
@@ -0,0 +1,37 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+    private int result = 0;
+
+public int Add(string numbers)
+    {
+        
+        if (numbers == "")
+        {
+            //empty string:
+            result = 0;
+        }
+        else if (numbers.Length == 1)
+        {
+            //single digit:
+            result = int.Parse(numbers);
+
+        }
+        else if (numbers.Substring(0,2) == "//")
+        {
+            //custom delimiter:
+            Char cd = numbers.ToCharArray().ElementAt(2);
+            string onlyNumbers = numbers.Substring(4);
+            result = onlyNumbers.Split(new Char[] { ',', '\n', cd }, StringSplitOptions.RemoveEmptyEntries).ToList().Select(x => int.Parse(x)).Sum();
+        }
+        else
+        {
+            //no custom delimiter:
+            result = numbers.Split(new Char[] { ',', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList().Select(x => int.Parse(x)).Sum();
+        }
+
+        return result;
+    }
+}
---
+++ Practices/stringcalculator-master/StringCalculatorTests.cs
@@ -0,0 +1,129 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void SingleDigit()
+    {
+        var calculator = new StringCalculator();
+
+        var singleDigit = "5";
+
+        var result = calculator.Add(singleDigit);
+
+        Assert.Equal(int.Parse(singleDigit), result);
+    }
+
+    // commas only :
+
+    [Fact]
+    public void TwoSingleDigitsWithOnlyCommas()
+    {
+        var calculator = new StringCalculator();
+
+        var twoSingleDigit = "1,2";
+
+        var result = calculator.Add(twoSingleDigit);
+
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void TwoDoubleDigitsWithOnlyCommas()
+    {
+        var calculator = new StringCalculator();
+
+        var twoDoubleDigit = "15,25";
+
+        var result = calculator.Add(twoDoubleDigit);
+
+        Assert.Equal(40, result);
+    }
+
+    [Fact]
+    public void FiveVaryingDigitsWithOnlyCommas()
+    {
+        var calculator = new StringCalculator();
+
+        var fiveVaryingDigit = "15,25,3,8,20";
+
+        var result = calculator.Add(fiveVaryingDigit);
+
+        Assert.Equal(71, result);
+    }
+
+    // new lines only :
+
+    [Fact]
+    public void TwoSingleDigitsWithOnlyNewLines()
+    {
+        var calculator = new StringCalculator();
+
+        var twoSingleDigit = "1\n2";
+
+        var result = calculator.Add(twoSingleDigit);
+
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void TwoDoubleDigitsWithOnlyNewLines()
+    {
+        var calculator = new StringCalculator();
+
+        var twoDoubleDigit = "15\n25";
+
+        var result = calculator.Add(twoDoubleDigit);
+
+        Assert.Equal(40, result);
+    }
+
+    [Fact]
+    public void FiveVaryingDigitsWithOnlyNewLines()
+    {
+        var calculator = new StringCalculator();
+
+        var fiveVaryingDigit = "15\n25\n3\n8\n20";
+
+        var result = calculator.Add(fiveVaryingDigit);
+
+        Assert.Equal(71, result);
+    }
+
+    //new lines or commas:
+
+    [Fact]
+    public void FiveVaryingDigitsWithCommasAndNewLines()
+    {
+        var calculator = new StringCalculator();
+
+        var fiveVaryingDigit = "15\n25,3\n8,20";
+
+        var result = calculator.Add(fiveVaryingDigit);
+
+        Assert.Equal(71, result);
+    }
+
+    [Fact]
+    public void easyCustomDelimiter()
+    {
+        var calculator = new StringCalculator();
+
+        var customDelimiter = "//+\n15,25+3\n5";
+
+        var result = calculator.Add(customDelimiter);
+
+        Assert.Equal(48, result);
+    }
+}
