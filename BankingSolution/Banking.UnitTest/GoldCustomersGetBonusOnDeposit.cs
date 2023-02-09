9e25be88fab30b24016e4046c1bfe864c807d4e5
Jared Zahn <jared_zahn@progressive.com>
2023-02-09 11:23:45 -0500

    inheritance branch

---
+++ BankingSolution/Banking.UnitTest/GoldCustomersGetBonusOnDeposit.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Banking.UnitTests;
+
+public class GoldCustomersGetBonusOnDeposit
+{
+
+    [Fact]
+    public void BonusAppliedToDeposit()
+    {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
+    }
+
+
+}
