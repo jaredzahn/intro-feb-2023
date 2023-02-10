55cd42f994cb947f518faf8603da05f093510c00
Jared Zahn <jared_zahn@progressive.com>
2023-02-10 14:47:23 -0500

    minor update

--- Practices/stringcalculator-friday/StringCalculator.cs
+++ Practices/stringcalculator-friday/StringCalculator.cs
@@ -1,11 +1,25 @@
-﻿
-namespace StringCalculator;
+﻿namespace StringCalculator; 
 
 public class StringCalculator
 {
 
+    private ILogger 
+
     public int Add(string numbers)
     {
-        return -42;
+        int answer = 0;
+        if (numbers == "")
+        {
+            answer = 0;
+        }
+        else
+        {
+            answer = numbers.Split(',', '\n')
+            .Select(int.Parse)
+            .Sum();
+        }
+        // WTCYWYH
+        _logger.Write(answer.ToString());
+        return answer;
     }
 }
