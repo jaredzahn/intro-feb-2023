18fb90171e51cdd0f659fbae8ad1a3fd128c51f6
Jared Zahn <jared_zahn@progressive.com>
2023-02-10 12:30:43 -0500

    test update.

--- BankingSolution/Banking.UnitTest/AccountTransactionsGuards.cs
+++ BankingSolution/Banking.UnitTest/AccountTransactionsGuards.cs
@@ -4,16 +4,18 @@ using Moq;
 namespace Banking.UnitTests;
 public class AccountTransactionsGuards
 {
-    // We have a defect (there will usually be some ID associated with it)     
+    // We have a defect (there will usually be some ID associated with it)  
+       
     // Step 1: Recreate the defect. Write a test that passes because it demonstrates the bad behavior.
-    [Fact]
-    public void AllowNegativeNumbers()
-    {
-        var stubbedBonusCalculator = new Mock<ICanCalculateAccountBonuses>();
-        var account = new BankAccount(stubbedBonusCalculator.Object);
-        var openingBalance = account.GetBalance();
-        account.Deposit(-1000); Assert.Equal(openingBalance - 1000, account.GetBalance());
-    }     
+    //[Fact]
+    //public void AllowNegativeNumbers()
+    //{
+    //    var stubbedBonusCalculator = new Mock<ICanCalculateAccountBonuses>();
+    //    var account = new BankAccount(stubbedBonusCalculator.Object);
+    //    var openingBalance = account.GetBalance();
+    //    account.Deposit(-1000); Assert.Equal(openingBalance - 1000, account.GetBalance());
+    //}
+     
     // Step 2: Write another test that demonstrates how it SHOULD work if the defect is fixed. It should fail.
     [Fact]
     public void NegativeNumbertsNotAllowed()
