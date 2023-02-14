2f67a89b8158dce52277c10a2a3fec7aef8d74d7
Jared Zahn <jared_zahn@progressive.com>
2023-02-14 16:34:00 -0500

    quick update for enum.toString

--- LearningResourcesSolution/LearningResourcesApi/Controllers/ResourcesController.cs
+++ LearningResourcesSolution/LearningResourcesApi/Controllers/ResourcesController.cs
@@ -1,4 +1,5 @@
 ﻿using LearningResourcesApi.Adapters;
+using LearningResourcesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,16 @@ public class ResourcesController : ControllerBase
     [HttpGet("/resources")]
     public async Task<ActionResult> GetResources()
     {
-        var response = await _context.Items.ToListAsync();
+        var items = await _context.Items
+            .Select(item => new GetResourceItem
+            {
+                Id = item.Id.ToString(),
+                Description = item.Description,
+                Link = item.Link,
+                Type = item.Type
+            }).ToListAsync();
+
+        var response = new GetResourcesResponse { Items = items };
         return Ok(response);
     }
 
